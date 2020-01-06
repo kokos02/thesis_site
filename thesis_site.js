062fa845ea147749be996191a06ffc0deefc7c45
unknown <csd3727@csd.uoc.gr>
2020-01-06 23:56:47 +0200

    ajax works

--- thesis_site.js
+++ thesis_site.js
@@ -1,12 +1,19 @@
-function myFunction() {
+var tools = [];
+var cases = [];
 
-    alert("ouuuuuuuuuuuu");
+
+
+
+function display_cases() {
     
     var case_number;
+   
 
     for (case_number = 1; case_number < 30; case_number++) {
+
+      tools[case_number]=case_number;
         $("#photo_video").append(" <div class=\"col-4\">\
-    <h3 style=\"font-size:3vw;\">Case study " + case_number+ "</h3>\
+    <h3 style=\"font-size:3vw;\">" + cases[case_number] + "</h3>\
 \
     <figure class=\"figure\">\
       <img src=\"tattoo.png\" class=\"img-fluid figure-img axis_images\">\
@@ -34,4 +41,38 @@ function myFunction() {
   <br>")
 
 }
+}
+
+
+function receive_and_display(){
+  $(document).ready(function(){
+    /* call the php that has the php array which is json_encoded */
+    $.getJSON('thesis_site.php', function(data) {
+            /* data will hold the php array as a javascript object */
+            $.each(data, function(key, val) {
+              cases[key] = val;
+            });
+            display_cases();
+    });
+
+});
+ /* alert(str);
+  if(str.length == 0){
+    document.getElementById('output').innerHTML = '';
+    alert("lenght 0");
+  } else {
+    // AJAX REQ
+    alert("lenght !=0");
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function(){
+      if(this.readyState == 4 && this.status == 200){ 
+        alert("ready")
+        var array = JSON.parse(this.responseText);
+        alert(array[0]);
+      }
+    }
+    xmlhttp.open("GET", "thesis_site.php?q="+str, true);
+    xmlhttp.send();
+  }*/
+  
 }
---
+++ thesis_site.php
@@ -0,0 +1,69 @@
+<?php
+$file = 'thesis.txt';
+$case = array();
+
+
+
+// the following line prevents the browser from parsing this as HTML.
+header('Content-Type: text/plain');
+
+// get the file contents, assuming the file to be readable (and exist)
+$contents = file_get_contents($file);
+
+for ($x = 1; $x <= 51; $x++) {
+    $searchfor = 'Case study '. $x .' :';
+
+    // escape special characters in the query
+$pattern = preg_quote($searchfor, '/');
+// finalise the regular expression, matching the whole line
+$pattern = "/^.*$pattern.*\$/m";
+// search, and store all matching occurences in $matches
+if(preg_match_all($pattern, $contents, $matches)){
+   //echo "Found matches:\n";
+   //echo implode("\n", $matches[0]);
+   $case[$x] = implode("\n", $matches[0]);
+}
+else{
+   echo "No matches found";
+}
+
+}
+
+//$q = $_REQUEST['q'];
+/*
+$suggestion = "";
+
+// Get Suggestions
+if($q !== ""){
+	$q = strtolower($q);
+	$len = strlen($q);
+	foreach($case as $person){
+		if(stristr($q, substr($person, 0, $len))){
+			if($suggestion === ""){
+				$suggestion = $person;
+			} else {
+				$suggestion .= ", $person";
+			}
+		}
+	}
+}
+*/
+//echo json_encode($case);
+$arr = array(
+	array(
+			"first_name" => "Darian",
+			"last_name" => "Brown",
+			"age" => "28",
+			"email" => "darianbr@example.com"
+	),
+	array(
+			"first_name" => "John",
+			"last_name" => "Doe",
+			"age" => "47",
+			"email" => "john_doe@example.com"
+	)
+);
+
+/* encode the array as json. this will output [{"first_name":"Darian","last_name":"Brown","age":"28","email":"darianbr@example.com"},{"first_name":"John","last_name":"Doe","age":"47","email":"john_doe@example.com"}] */
+echo json_encode($case);
+?>
